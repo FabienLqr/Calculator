345e9c61e7bd0636e01649dd4fc9390018cb62d9
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-06 13:23:08 +0200

    start js-> take element

---
+++ app.js
@@ -0,0 +1,10 @@
+const touches = [...document.querySelectorAll(".bouton")];
+const listeKeyCode = touches.map((touche) => touche.dataset.key);
+
+document.addEventListener("keydown", (e) => {
+  const valeur = e.keyCode.toString();
+});
+
+document.addEventListener("click", (e) => {
+  const valeur = e.target.dataset.key;
+});
